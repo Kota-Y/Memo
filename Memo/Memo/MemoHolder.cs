4ac43d71633d79c0f46b28912eacf21d673f5e60
Kota-Y <mcz-0604-htt@outlook.com>
2017-10-08 21:50:56 +0900

    add

---
+++ Memo/Memo/MemoHolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memo
+{
+    public class MemoHolder
+    {
+        public static MemoHolder Current { get; } = new MemoHolder();
+        public Memo Memo { get; set; }
+    }
+}
